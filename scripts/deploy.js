4082724cdca32f35b6941d52b2575604cee8b4d0
Chandana.Boge <bogechandana1210@gmail.com>
2025-07-28 10:57:28 +0530

    Create deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const initialSupply = ethers.utils.parseUnits("1000000", 18);
+  const Token = await ethers.getContractFactory("MyToken");
+  const token = await Token.deploy(initialSupply);
+  await token.deployed();
+  console.log("Token deployed to:", token.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+ 
